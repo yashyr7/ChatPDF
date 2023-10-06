ab1d28e951a2bbf3db1e2f0161620784c4825987
Yash Rathore <yashrathore@Yashs-MBP.lan>
2023-10-06 03:27:19 -0400

    initial commit

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from llama_index import VectorStoreIndex, StorageContext, download_loader, ServiceContext, set_global_service_context
 from llama_index.llms import OpenAI
 
-os.environ["OPENAI_API_KEY"] = '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'
+os.environ["OPENAI_API_KEY"] = 'API_KEY'
 
 # define LLM
 llm = OpenAI(model="gpt-4", temperature=0, max_tokens=256)
@@ -14,7 +14,7 @@ set_global_service_context(service_context)
 
 PDFMinerReader = download_loader("PDFMinerReader")
 loader = PDFMinerReader()
-documents = loader.load_data(file=Path('./termtest1.pdf'))
+documents = loader.load_data(file=Path('./Your_PDF.pdf'))
 index = VectorStoreIndex.from_documents(documents, show_progress=True)
 index.storage_context.persist(persist_dir="./db/")
 
